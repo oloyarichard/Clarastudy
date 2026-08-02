fdb816bddbb12216f21f2c798e2d159fc009b6d3
Oloya Richard Okidi <oloyarichard2005@gmail.com>
2026-08-02 08:13:10 +0300

    Good2

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -9,7 +9,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,,
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
